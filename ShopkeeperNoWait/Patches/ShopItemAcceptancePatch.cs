f56d1948ed72a0db8f39a03084c52f0b6dab8807
Aaron Jesus <sant5050@aol.com>
2023-12-09 13:12:07 -0500

    Add project files.

---
+++ ShopkeeperNoWait/Patches/ShopItemAcceptancePatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopkeeperNoWait.Patches
+{
+    [HarmonyPatch(typeof(DepositItemsDesk))]
+    internal class ShopItemAcceptancePatch
+    {
+        [HarmonyPatch(nameof(DepositItemsDesk.SetTimesHeardNoiseServerRpc))]
+        [HarmonyPostfix]
+        static void noDelayPatch(ref float ___timesHearingNoise)
+        {
+            ___timesHearingNoise = 8f;
+        }
+    }
+}
---
+++ ShopkeeperNoWait/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ShopkeeperNoWait.Patches;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopkeeperNoWait
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class ShopkeerNoWaitModBase : BaseUnityPlugin
+    {
+        private const string modGUID = "Sant.ShopkeeperWait";
+        private const string modName = "Shopkeeper Wait Time";
+        private const string modVersion = "1.0.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static ShopkeerNoWaitModBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("The Shopkeepr wait mod has awaken :)");
+
+            harmony.PatchAll(typeof(ShopkeerNoWaitModBase));
+            harmony.PatchAll(typeof(ShopItemAcceptancePatch));
+        }
+    }
+}
